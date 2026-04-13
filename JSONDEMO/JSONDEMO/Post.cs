ed9c95c2a50b84c4d2d08b3d69471b1ce2daaad4
Nhlanhla Mabasa <107392458+NhlanhlaMa@users.noreply.github.com>
2026-04-13 11:43:25 +0200

    Add files via upload

---
+++ JSONDEMO/JSONDEMO/Post.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONDEMO
+{
+    public class Post
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+}
---
+++ JSONDEMO/JSONDEMO/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Newtonsoft.Json;
+
+namespace JSONDEMO
+{
+ 
+
+    internal class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+            string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+            HttpClient httpClient = new HttpClient();
+
+           
+            try
+            {
+                var httpResponseMessage = await httpClient.GetAsync(url);
+
+                // read the string from the response's content.
+                string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+                
+                //Print the json response
+                Console.WriteLine($"Json Structure \n {jsonResponse} \n");
+
+                // Deserialize the json response into a C# array of type Post[]
+                var myPosts = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+
+                Console.WriteLine($"Deserialized Data");
+                // Print the array
+                foreach (var post in myPosts)
+                {
+                    //print the id and the title of each post
+                    Console.WriteLine($"{post.Id} {post.Title}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+        }
+    }
+}
